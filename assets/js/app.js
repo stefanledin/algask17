443acc066b1d3d01f149772ac7af2bf2d7a27852
Stefan Ledin <stefan.ledin@gmail.com>
2017-01-03 07:05:56 +0100

    jobbat med headern mobilt. börjat med artiklar

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+(function (window, document, undefined) {
+
+    var body = document.body;
+
+    var mobileMenu = function () {
+        var openButton = document.querySelector('.site-header__mobile-menu-button');
+        var closeButton = document.querySelector('button.mobile-menu__close-button');
+        var overlay = document.getElementById('canvas-content-overlay');
+        
+        var openClose = function (event) {
+            event.preventDefault();
+            body.classList.toggle('mobile-menu-is-open');
+        };
+
+        openButton.addEventListener('click', openClose);
+        openButton.addEventListener('touch', openClose);
+        closeButton.addEventListener('click', openClose);
+        closeButton.addEventListener('touch', openClose);
+        overlay.addEventListener('click', openClose);
+        overlay.addEventListener('touch', openClose);
+    };
+    mobileMenu();
+
+})(window, document);
--- index.php
+++ index.php
@@ -11,28 +11,75 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class();?>>
-        
-        <header class="site-header">
-            <div class="site-width">
-                <div class="site-header__logo-wrapper">
-                    <div class="site-header__logo-symbol">
-                        <picture>
-                            <source type="image/svg+xml" srcset="<?php echo asset('img/algask-klubbmarke.svg');?>">
-                            <img src="<?php echo asset('img/algask-klubbmarke.png');?>" alt="Älgå Sportklubb">
-                        </picture>
-                    </div>
-                    <div class="site-header__logo-text">
-                        <picture>
-                            <source type="image/svg+xml" srcset="<?php echo asset('img/algask-klubbmarke-text.svg');?>">
-                            <img src="<?php echo asset('img/algask-klubbmarke-text.png');?>" alt="Älgå Sportklubb">
-                        </picture>
-                    </div>
-                </div>
-                <div class="site-header__mobile-menu-button-wrapper">
-                    <button class="site-header__mobile-menu-button">Meny</button>
+
+        <div class="page-wrapper">
+            <div class="mobile-menu">
+                <div class="inner">
+                    <button class="mobile-menu__close-button">Stäng</button>
+                    <ul>
+                        <li><a href="#">Plask</a></li>
+                        <li><a href="#">Plask</a></li>
+                        <li><a href="#">Plask</a></li>
+                        <li><a href="#">Plask</a></li>
+                        <li><a href="#">Plask</a></li>
+                    </ul>
                 </div>
             </div>
-        </header>
+            
+            <div class="page-content">
+                <div id="canvas-content-overlay"></div>
+                
+                <header class="site-header">
+                    <div class="site-width flex vertical-center">
+                        <div class="site-header__logo-wrapper vertical-center">
+                            <div class="site-header__logo-symbol">
+                                <a href="<?php echo home_url();?>">
+                                    <picture>
+                                        <source type="image/svg+xml" srcset="<?php echo asset('img/algask-klubbmarke.svg');?>">
+                                        <img src="<?php echo asset('img/algask-klubbmarke.png');?>" alt="Älgå Sportklubb">
+                                    </picture>
+                                </a>
+                            </div>
+                            <div class="site-header__logo-text">
+                                <a href="<?php echo home_url();?>">
+                                    <picture>
+                                        <source type="image/svg+xml" srcset="<?php echo asset('img/algask-klubbmarke-text.svg');?>">
+                                        <img src="<?php echo asset('img/algask-klubbmarke-text.png');?>" alt="Älgå Sportklubb">
+                                    </picture>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="site-header__mobile-menu-button-wrapper">
+                            <button class="site-header__mobile-menu-button">Meny</button>
+                        </div>
+                    </div>
+                </header>
+
+                <section class="section-wrapper">
+                    <div class="site-width">
+                        <div class="article-list">
+                            <?php if ( have_posts() ) : $i = 0; while ( have_posts() ) : the_post(); ?>
+                            <article class="article-list__article">
+                                <header class="article-list__article__header">
+                                    <time><?php the_time( 'Y-m-d H:i' ); ?></time>
+                                    <h2 class="article-list__article__title">
+                                        <a href="<?php the_permalink();?>"><?php the_title();?></a>
+                                    </h2>
+                                </header>
+                                <?php if ( $i == 0 ) : ?>
+                                    <?php the_post_thumbnail( ); ?>
+                                    <?php the_content(); ?>
+                                <?php endif; ?>
+                            </article>
+                            <hr class="article-list__separator">
+                            <?php $i++; endwhile; endif; ?>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+        </div>
+        
 		
         <?php wp_footer(); ?>
 	</body>
