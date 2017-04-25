55576be2e263c5d44162d8205b7c78e413e28abd
Stefan Ledin <stefan.ledin@gmail.com>
2017-04-25 07:09:01 +0200

    custom fields. bilder

--- includes/images/custom-image-sizes.php
+++ includes/images/custom-image-sizes.php
@@ -1,8 +1,27 @@
 <?php
-// Tablet portrait
-add_image_size( 'tablet-portrait', '880', '980', false );
-// iPhone landscape
-add_image_size( 'iphone-landscape', '480', '580', false );
+// 375 (750)
+// [Thumbnail]
+add_image_size( 'thumbnail@2x', 750 );
+
+// 480 (960)
+add_image_size( 'size480', 480 );
+add_image_size( 'size480@2x', 960 );
+
+// 568 (1136)
+// [Medium]
+add_image_size( 'medium@2x', 1136 );
+
+// 667 (1334)
+add_image_size( 'size667', 667 );
+add_image_size( 'size667@2x', 1334 );
+
+// 768 (1536)
+add_image_size( 'size768', 768 );
+add_image_size( 'size768@2x', 1536 );
+
+// 1024 (2048)
+// [Large]
+add_image_size( 'large@2x', 2048 );
 
 // Boilerplate
 // add_image_size( $name, $width, $height, $crop );
