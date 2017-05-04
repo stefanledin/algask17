5ec8675608297eb889f9985691d7d60464347df3
Stefan Ledin <stefan.ledin@gmail.com>
2017-05-04 06:46:23 +0200

    påbörjat single-spelare

---
+++ single-spelare.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+<main class="main-column right has-shadow">
+    <div class="inner">
+        <h1><?php the_title(); ?></h1>
+        <div class="player">
+            <figure class="player__image">
+                <img src="http://placehold.it/185x277" alt="">
+            </figure>
+            <table class="player__info">
+                <tbody>
+                    <tr>
+                        <td>Position</td>
+                        <td>Mittfält</td>
+                    </tr>
+                    <tr>
+                        <td>Född</td>
+                        <td>2001</td>
+                    </tr>
+                    <tr>
+                        <td>Moderklubb</td>
+                        <td>Älgå SK</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
