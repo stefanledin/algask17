bc766a40719f0f57d646624d7b3f9771eedfa957
Stefan Ledin <stefan.ledin@gmail.com>
2017-05-04 08:40:20 +0200

    ACF-lösning för lagsida

--- team.php
+++ team.php
@@ -8,21 +8,25 @@ get_header();
 <main class="main-column right has-shadow">
     <div class="inner">
         <h1><?php the_title();?></h1>
-        <h2>Målvakter</h2>
+        <?php if ( have_rows('player_list') ) : while ( have_rows('player_list') ) : the_row(); ?>
+        <h2><?php the_sub_field('position');?></h2>
         <ul class="player-list">
-            <?php $i = 0; while ( $i < 7 ) : ?>
-            <li class="player-list__item">
-                <figure>
-                    <img src="http://placehold.it/185x277" alt="">
-                    <?php if ( $i % 2 == 0 ) : ?>
-                        <figcaption>Namn Namnsson</figcaption>
-                    <?php else : ?>
-                        <figcaption>Namn Långnamn Namnsson</figcaption>
-                    <?php endif; ?>
-                </figure>
-            </li>
-            <?php $i++; endwhile; ?>
+            <?php if ( have_rows('players') ) : while( have_rows('players') ) : the_row(); ?>
+                <?php
+                foreach ( get_sub_field('player') as $player ) :
+                ?>
+                <li class="player-list__item">
+                    <a href="<?php echo get_permalink( $player->ID );?>">
+                        <figure>
+                            <img src="http://placehold.it/185x277" alt="">
+                            <figcaption><?php echo $player->post_title;?></figcaption>
+                        </figure>
+                    </a>
+                </li>
+                <?php endforeach; ?>
+            <?php endwhile; endif; ?>
         </ul>
+        <?php endwhile; endif; ?>
     </div>
 </main>
 
