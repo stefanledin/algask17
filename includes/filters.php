5797974814dc40ba63650b3976862c1af97c2c45
Stefan Ledin <stefan.ledin@gmail.com>
2017-07-24 13:03:56 +0200

    More jump

--- includes/filters.php
+++ includes/filters.php
@@ -38,3 +38,18 @@ function defer_home() {
     return false;
 }
 add_filter( 'autoptimize_filter_css_defer', 'defer_home', 10, 0 );
+
+/*
+Ta bort more-hoppet
+ */
+function remove_more_jump_link($link) { 
+    $offset = strpos($link, '#more-');
+    if ($offset) {
+        $end = strpos($link, '"',$offset);
+    }
+    if ($end) {
+        $link = substr_replace($link, '', $offset, $end-$offset);
+    }
+    return $link;
+}
+add_filter('the_content_more_link', 'remove_more_jump_link');
